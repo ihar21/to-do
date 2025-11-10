bb082498a648a6612fcff9b5d34304e73d2a5272
ihar21 <gogakharitonov@gmail.com>
2025-11-11 01:33:40 +0200

    form and model

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine("sqlite:///tasks.db", echo = True)
+Base = declarative_base()
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key = True)
+    name = Column(String)
+    description = Column(String)
+    due_date = Column(DateTime)
+    priority = Column(Integer)
+    done = Column(Boolean, default = False)
+
+    def __repr__(self):
+        return f"<Task(id={self.id}), name={self.name}, description={self.description}"
+
+Base.metadata.create_all(engine)
